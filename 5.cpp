f9b56824228bd30156a753b25f0c3c23141d4986
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-29 20:09:52 +0100

    .
    Uso la inferencia de tipos auto para declarar variables

--- 5.cpp
+++ 5.cpp
@@ -4,6 +4,12 @@
 #include <algorithm>
 
 int main() {
-   
+
     auto cuadrado = [](int x) { return x * x; };
 
+
+    auto numero = 5;
+    auto resultado = cuadrado(numero);
+    std::cout << "El cuadrado de " << numero << " es: " << resultado << std::endl;
+
+  
