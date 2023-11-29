e4477d91d5e401352c652e6c06df322304d8b98b
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-29 19:59:02 +0100

    .
    ordeno el vector

--- 3.cpp
+++ 3.cpp
@@ -3,4 +3,13 @@
 #include <algorithm>
 
 int main() {
-    std::vector<int> miVector = {14, 3, 7, 10, 1, 20}; 
+    std::vector<int> miVector = {14, 3, 7, 10, 1, 20};
+
+    std::sort(miVector.begin(), miVector.end());
+    
+    std::cout << "Vector ordenado: ";
+    for (const auto& num : miVector) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
