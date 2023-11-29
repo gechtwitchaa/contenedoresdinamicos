e482642e693ee0038f49029b165da6debcadc717
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-29 20:03:48 +0100

    .
    suma y resta

--- 4.cpp
+++ 4.cpp
@@ -2,7 +2,12 @@
 #include <valarray>
 
 int main() {
-   
+
     std::valarray<double> valArray1 = {1.5, 2.5, 3.5, 4.5};
     std::valarray<double> valArray2 = {0.5, 1.0, 1.5, 2.0};
 
+    
+    std::valarray<double> suma = valArray1 + valArray2;
+
+
+    std::valarray<double> resta = valArray1 - valArray2;
