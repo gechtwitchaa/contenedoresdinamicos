bb6f8d6311ff5f7b77c56a7072d397120ffbb191
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-29 19:46:41 +0100

    .
    uso el iterador

--- 1.cpp
+++ 1.cpp
@@ -3,9 +3,17 @@
 
 int main() {
     std::vector<int> miVector;
-    
+
     miVector.push_back(5);
     miVector.push_back(10);
     miVector.push_back(15);
     miVector.push_back(20);
+    
+    std::cout << "Elementos del vector:" << std::endl;
+    for (std::vector<int>::iterator it = miVector.begin(); it != miVector.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 
+    return 0;
+}
