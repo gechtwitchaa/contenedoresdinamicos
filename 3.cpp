4242c27f76a2ddb60828cda55863c3f5ff9e3321
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-29 19:59:47 +0100

    .
    encuentro el elemento mas grande del vector usando max element

--- 3.cpp
+++ 3.cpp
@@ -6,10 +6,20 @@ int main() {
     std::vector<int> miVector = {14, 3, 7, 10, 1, 20};
 
     std::sort(miVector.begin(), miVector.end());
-    
+
     std::cout << "Vector ordenado: ";
     for (const auto& num : miVector) {
         std::cout << num << " ";
     }
     std::cout << std::endl;
+    
+    auto maxElement = std::max_element(miVector.begin(), miVector.end());
 
+    if (maxElement != miVector.end()) {
+        std::cout << "El elemento más grande del vector es: " << *maxElement << std::endl;
+    } else {
+        std::cout << "El vector está vacío." << std::endl;
+    }
+
+    return 0;
+}
