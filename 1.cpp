4cf9c4fb947cd45a6825cfa08c3e95fea5094218
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-29 19:49:07 +0100

    .

--- 1.cpp
+++ 1.cpp
@@ -8,7 +8,7 @@ int main() {
     miVector.push_back(10);
     miVector.push_back(15);
     miVector.push_back(20);
-    
+
     std::cout << "Elementos del vector:" << std::endl;
     for (std::vector<int>::iterator it = miVector.begin(); it != miVector.end(); ++it) {
         std::cout << *it << " ";
