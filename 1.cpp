7f8e2806b4da798846324a5b9db5e6ba2db55747
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-29 19:46:12 +0100

    .
    añado elementos al vector

--- 1.cpp
+++ 1.cpp
@@ -2,4 +2,10 @@
 #include <vector>
 
 int main() {
-    std::vector<int> miVector; 
+    std::vector<int> miVector;
+    
+    miVector.push_back(5);
+    miVector.push_back(10);
+    miVector.push_back(15);
+    miVector.push_back(20);
+
