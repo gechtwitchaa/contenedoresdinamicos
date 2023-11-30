3a7d8f36b153809a33b216b0a1d2b10c0650ab9a
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-30 12:28:42 +0100

    .

--- 6.cpp
+++ 6.cpp
@@ -7,4 +7,12 @@ int main() {
     std::cout << "Ingrese una cadena de números: ";
     std::cin >> cadena;
 
-  
+    try {
+        int numero = boost::lexical_cast<int>(cadena);
+        std::cout << "Número convertido: " << numero << std::endl;
+    } catch (const boost::bad_lexical_cast &e) {
+        std::cerr << "Error de conversión: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
