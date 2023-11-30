31e085e7239478f59591711204e276d13cc52d88
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-30 12:28:24 +0100

    .
    simular la entrada de una cadena

--- 6.cpp
+++ 6.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <boost/lexical_cast.hpp>
 
+int main() {
+    std::string cadena;
+
+    std::cout << "Ingrese una cadena de números: ";
+    std::cin >> cadena;
+
+  
